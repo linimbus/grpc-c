2d4d3b9823d09fcafe68160f2adcae90725b8bc7
攻城狮 <linimbus@126.com>
2019-02-28 22:13:30 +0800

    Update builddeps.sh

--- builddeps.sh
+++ builddeps.sh
@@ -4,17 +4,11 @@
 #
 
 #!/bin/sh
-pfix=/usr/local
+pfix=/usr/
 if [ $# -gt 0 ]
 then
     pfix=$1
 fi
-
-echo "Building gRPC"
-cd third_party/grpc
-git submodule update --init
-make && sudo make prefix=${pfix} install
-cd ../../
 
 echo "Installing Protobuf"
 cd third_party/protobuf
@@ -27,3 +21,8 @@ cd third_party/protobuf-c
 ./autogen.sh && ./configure --prefix=${pfix} && make && sudo make install
 cd ../../
 
+echo "Building gRPC"
+cd third_party/grpc
+git submodule update --init
+make && sudo make prefix=${pfix} install
+cd ../../
