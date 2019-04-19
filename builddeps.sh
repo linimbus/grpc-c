feeb7afdf539f42ca32b5b546dd1f43424c6a9b4
lixiangyun <linimbus@126.com>
2019-04-19 23:04:34 +0800

    update

--- builddeps.sh
+++ builddeps.sh
@@ -4,26 +4,39 @@
 #
 
 #!/bin/sh
-pfix=/usr/local
+PERFIX_PATH=/usr/local
 if [ $# -gt 0 ]
 then
-    pfix=$1
+    PERFIX_PATH=$1
 fi
 
 chmod -R +x *
 
+echo "Building C-ares"
+cd third_party/c-ares
+./configure --prefix=${PERFIX_PATH}
+make -j 4
+make install
+cd ../../
+
 echo "Installing Protobuf"
 cd third_party/protobuf
-./autogen.sh && ./configure --prefix=${pfx} && make -j 4&& sudo make prefix=${pfix} install
+./autogen.sh
+./configure --prefix=${pfx}
+make -j 4
+make prefix=${PERFIX_PATH} install
 sudo ldconfig
 cd ../../
 
 echo "Building protobuf-c"
 cd third_party/protobuf-c
-./autogen.sh && ./configure --prefix=${pfix} && make -j 4&& sudo make install
+./configure --prefix=${PERFIX_PATH}
+make -j 4
+make install
 cd ../../
 
 echo "Building gRPC"
 cd third_party/grpc
-make && sudo make prefix=${pfix} install
+make -j 4
+make prefix=${PERFIX_PATH} install
 cd ../../
