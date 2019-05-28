8108afef56d492aa8d69240e242105bb886fb270
攻城狮 <linimbus@126.com>
2019-05-28 17:25:50 +0800

    Update foo_main.c

--- examples/foo/foo_main.c
+++ examples/foo/foo_main.c
@@ -8,19 +8,14 @@ int foo_server();
 int foo_client();
 
 int main(int argv, char ** args) {
-    
     if ( argv < 2) {
         printf("usage: <server/client>\n");
         return -1;
     }
-    
     if ( 0 == strcmp(args[1],"server") ) {
         foo_server();
     }else {
         foo_client();
     }
-
-    sleep(1);
-    
     return 0;
 }
